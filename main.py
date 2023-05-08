120b36006ee9e631c578f7001af76376017f4094
Alex Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2023-05-08 19:32:17 -0400

    I am so fucking done with this right now, I just can't

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+from screeninfo import get_monitors
 
+
+window_size = (8.0, 4.5)
+for m in get_monitors():
+    if m.is_primary:
+        window_size = (m.width_mm / 25.4, m.height_mm / 25.4)
+
+width = 0.5
+euler_alpha = .9
 
 g = -9.81
 k_air = 0.0035
@@ -10,6 +19,36 @@ vel_to_mph = 2.23694
 parachute_height = 1000
 openning_distance = 100
 altitude_constant = 0.9999
+
+
+class LinearInterpolation(sp.integrate.DenseOutput):
+    def __init__(self, t_old, t, y_old, y):
+        super().__init__(t_old, t)
+        self.y_old = y_old
+        self.y = y
+    
+    def _call_impl(self, t):
+        if len(t.shape) > 0:
+            return np.array([self.y_old + (i - self.t_min) / (self.t_max - self.t_min) * (self.y - self.y_old) for i in t])
+        return self.y_old + (t - self.t_min) / (self.t_max - self.t_min) * (self.y - self.y_old)
+
+
+class ForwardEuler(sp.integrate.OdeSolver):
+    def __init__(self, fun, t0, y0, t_bound, vectorized, support_complex=False, **extraneous):
+        super().__init__(fun, t0, y0, t_bound, vectorized, support_complex)
+        self.y_old = None
+        self.h = 0.5
+
+    def _step_impl(self) -> tuple[bool, None]:
+        self.y_old = self.y.copy()
+        derivative = self.fun(self.t, self.y)
+        self.y = self.y + derivative * self.h
+        self.t += self.h
+        return (True, None)
+
+    def _dense_output_impl(self):
+        return LinearInterpolation(self.t_old, self.t, self.y_old, self.y)
+
 
 def air_resistance(t: float, y: np.ndarray) -> np.ndarray:
     return np.array([y[1], g + k_air * y[1]**2])
@@ -56,8 +95,7 @@ part_three_conditions = (
 
 
 def simulate(conditions) -> None:
-    eval_points = np.linspace(0, 1e3, int(1e6))
-    
+    eval_points = np.arange(0, 1000, 0.5)
 
     def height(t: float, y: np.ndarray) -> float:
         return y[0]
@@ -67,29 +105,48 @@ def simulate(conditions) -> None:
     def acceleration(t: float, y: np.ndarray) -> float:
         return conditions[1](t, y)[1]
 
-    solution = sp.integrate.solve_ivp(conditions[1], (0, 1e6), conditions[0], t_eval=eval_points, events=[height, acceleration])
+    solution = sp.integrate.solve_ivp(conditions[1], (0, 1e6), conditions[0], method='RK23', t_eval=eval_points, events=[height, acceleration])
+    solution_euler = sp.integrate.solve_ivp(conditions[1], (0, 1e6), conditions[0], method=ForwardEuler, events=[height, acceleration])
 
-
+    print(f'Dormand Prince method took {solution.nfev} physics calculations.')
     times = solution.t
     states = solution.y
     fall_time = solution.t_events[0][0]
     landing_speed = np.abs(solution.y_events[0][0, 1])
-    print(f'Skydiver landed after {round(fall_time, 3)} seconds traveling at a speed of {round(landing_speed, 3)} m/s ({round(landing_speed * vel_to_mph, 3)} mph).')
+    print(f'Dormand Prince: Skydiver landed after {round(fall_time, 3)} seconds traveling at a speed of {round(landing_speed, 3)} m/s ({round(landing_speed * vel_to_mph, 3)} mph).')
     if len(solution.t_events[1]) != 0:
         max_vel_time = solution.t_events[1][0]
         max_vel_height = solution.y_events[1][0, 0]
         max_vel = np.abs(solution.y_events[1][0, 1])
-        print(f'At {round(max_vel_time, 3)} seconds the skydiver was falling fastest at an altitude of {round(max_vel_height * 1e-3, 3)} kilometers and a speed of {round(max_vel, 3)} m/s ({round(max_vel * vel_to_mph, 3)} mph).')
+        print(f'Dormand Prince: At {round(max_vel_time, 3)} seconds the skydiver was falling fastest at an altitude of {round(max_vel_height * 1e-3, 3)} kilometers and a speed of {round(max_vel, 3)} m/s ({round(max_vel * vel_to_mph, 3)} mph).')
+    
+    print(f'Euler method took {solution_euler.nfev} physics calculations.')
+    euler_times = solution_euler.t
+    euler_states = solution_euler.y
+    euler_fall_time = solution_euler.t_events[0][0]
+    euler_landing_speed = np.abs(solution_euler.y_events[0][0, 1])
+    print(f'Euler: Skydiver landed after {round(euler_fall_time, 3)} seconds traveling at a speed of {round(euler_landing_speed, 3)} m/s ({round(euler_landing_speed * vel_to_mph, 3)} mph).')
+    if len(solution_euler.t_events[1]) != 0:
+        euler_max_vel_time = solution_euler.t_events[1][0]
+        euler_max_vel_height = solution_euler.y_events[1][0, 0]
+        euler_max_vel = np.abs(solution_euler.y_events[1][0, 1])
+        print(f'Euler: At {round(euler_max_vel_time, 3)} seconds the skydiver was falling fastest at an altitude of {round(euler_max_vel_height * 1e-3, 3)} kilometers and a speed of {round(euler_max_vel, 3)} m/s ({round(euler_max_vel * vel_to_mph, 3)} mph).')
 
     heights = states[0, :]
     velocities = states[1, :]
 
-    _, (ax1, ax2) = plt.subplots(2, sharex=True)
+    euler_heights = euler_states[0, :]
+    euler_velocities = euler_states[1, :]
 
-    ax1.plot(times, heights)
+
+    _, (ax1, ax2) = plt.subplots(2, sharex=True, figsize=window_size)
+
+    ax1.plot(times, heights, c='r', lw=width)
+    ax1.plot(euler_times, euler_heights, alpha=euler_alpha, c='b', lw=width)
     ax1.set_title('Position')
 
-    ax2.plot(times, velocities)
+    ax2.plot(times, velocities, c='r', lw=width)
+    ax2.plot(euler_times, euler_velocities, alpha=euler_alpha, c='b', lw=width)
     ax2.set_title('Velocity')
 
     plt.show()
